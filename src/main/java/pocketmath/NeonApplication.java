5ba5bb77b9edd233d15c9b2f7579cb9b1e805fb8
Quang Mau Bach <quangmaubach@gmail.com>
2016-08-29 19:31:29 +0800

    push

--- src/main/java/pocketmath/NeonApplication.java
+++ src/main/java/pocketmath/NeonApplication.java
@@ -49,7 +49,8 @@ public class NeonApplication {
 
         //final int[] fixedEventsSize = new int[] {100, 150, 200, 350, 999, 1001, 1302, 1700, 1987, 2313, 8192, 12036};
         final int[] fixedEventsSize = new int[] {100, 150, 90, 110, 80, 120};
-        final int[] fixedBatchSize = new int[] {100, 200, 300, 500, 800, 900, 1000, 1200};
+        //final int[] fixedBatchSize = new int[] {100, 200, 300, 500, 800, 900, 1000, 1200};
+        final int[] fixedBatchSize = new int[] {100, 200, 300, 100, 200, 300, 100, 200, 300};
 
         for (int eventSize: fixedEventsSize) {
             for (int batchSize: fixedBatchSize) {
